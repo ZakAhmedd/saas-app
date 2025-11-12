18f7748d5ad71dbeb9efbff425d1130ef8ffeeab
Zak Ahmed <zakahmed2006@hotmail.com>
2025-11-12 00:25:18 +0000

    implement companion cards - homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
-import {Button} from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div className="text-2xl underline">
-      <h1>
-        Welcome
+    <main>
+      <h1 className="text-2xl underline">
+        Popular Companions
       </h1>
-      <Button>Click</Button>
-    </div>
+
+      <section className="home-section">
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          colour="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="maths"
+          duration={30}
+          colour="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          colour="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+
+    </main>
   )
 }
 

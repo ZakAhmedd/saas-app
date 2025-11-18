64b896f58de37bc3bd64221c9dc65bcf1dfd06d3
Zak Ahmed <zakahmed2006@hotmail.com>
2025-11-18 15:40:55 +0000

    implement supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "../components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" }} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );

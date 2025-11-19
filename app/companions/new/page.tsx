cbddd6b2c14f1a6c91e9ed53eb6e97a2d89eb2d4
Zak Ahmed <zakahmed2006@hotmail.com>
2025-11-19 17:53:03 +0000

    Implement auth, billing, database setup, create a companion server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -7,7 +7,7 @@ const NewCompanion = async() => {
   if ( !userId ) redirect("/sign-in")
 
   return (
-    <main className="lg:w-1/3 md:w-2/3 items-center justify-center">
+    <main className="lg:w-1/3 md:w-2/3 items-center justify-center mb-30">
       <article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
 

be838be989994af426977af483bd4aa5f0a01237
Zak Ahmed <zakahmed2006@hotmail.com>
2025-11-20 16:02:42 +0000

    Implement companion page

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,50 @@
-import CompanionCard from '@/components/CompanionCard'
-import CompanionsList from '@/components/CompanionsList'
-import CTA from '@/components/CTA'
-import { recentSessions } from '@/constants'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
     <main>
-      <h1 className="text-2xl underline">
-        Popular Companions
-      </h1>
+      <h1 className="text-2xl underline">Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionCard 
+        <CompanionCard
           id="123"
           name="Neura the Brainy Explorer"
           topic="Neural Network of the Brain"
           subject="science"
           duration={45}
-          colour="#ffda6e"
+          color="#ffda6e"
         />
-        <CompanionCard 
+        <CompanionCard
           id="456"
           name="Countsy the Number Wizard"
           topic="Derivatives and Integrals"
           subject="maths"
           duration={30}
-          colour="#e5d0ff"
+          color="#e5d0ff"
         />
-        <CompanionCard 
+        <CompanionCard
           id="789"
           name="Verba the Vocabulary Builder"
           topic="language"
           subject="English Literature"
           duration={30}
-          colour="#BDE7FF"
+          color="#BDE7FF"
         />
       </section>
 
       <section className="home-section">
-        <CompanionsList 
+        <CompanionsList
           title="Recently completed sessions"
           companions={recentSessions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
       </section>
-
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
